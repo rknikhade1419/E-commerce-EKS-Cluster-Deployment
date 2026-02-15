4270dee183bd3edab2af42cf7a8f86980a5644a8
rknikhade1419 <nikhaderoshankumar@gmail.com>
2026-02-15 12:42:52 +0530

    updated vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,19 +1,14 @@
-# vpc.tf
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "5.0.0"
 
-  name = "ecommerce-vpc"
+  name = "ecom-vpc"
   cidr = "10.0.0.0/16"
+  azs  = ["us-east-1a", "us-east-1b"]
 
-  azs             = ["us-east-1a", "us-east-1b"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
 
   enable_nat_gateway = true
-  single_nat_gateway = true # Cost-effective for dev
-
-  public_subnet_tags = {
-    "kubernetes.io/role/elb" = "1" # Required for Public ALBs
-  }
+  single_nat_gateway = true # Crucial for keeping costs low
 }
